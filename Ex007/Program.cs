fd9aa2c6d1f7364bddbe2542751ddf100ce1fab9
Pavel <n9032692530@gmail.com>
2022-08-19 21:31:39 +0300

    Задание №5

---
+++ Ex007/Program.cs
@@ -0,0 +1,11 @@
+﻿int n;
+Console.WriteLine("Введите число:");
+n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Выводим все четные числа");
+
+for(int x=n*(-1);x<=n; x++)
+    {
+        Console.Write(x);
+        if(x!=n) Console.Write(", ");
+        else Console.WriteLine(".");
+    }
