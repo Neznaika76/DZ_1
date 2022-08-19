5429568754974519f2bd62d89c7944a4e50e1fed
Pavel <n9032692530@gmail.com>
2022-08-19 21:16:14 +0300

    Задание №7

---
+++ Ex006/Program.cs
@@ -0,0 +1,8 @@
+﻿int n;
+int x;
+Console.WriteLine("Введите число:");
+n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Последняя цифра числа");
+x=n%10;
+Console.Write(x);
+Console.ReadKey();
