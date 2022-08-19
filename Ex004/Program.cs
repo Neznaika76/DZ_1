bcef22db48649d2cff0666187f4882d308c9c0c5
Pavel <n9032692530@gmail.com>
2022-08-19 20:51:53 +0300

    Задание №6

---
+++ Ex004/Program.cs
@@ -0,0 +1,12 @@
+﻿int x;
+Console.WriteLine("Введите число:");
+x=Convert.ToInt32(Console.ReadLine());
+if (x%2==0)
+    {
+        Console.WriteLine("Число четное");
+    }
+else
+   {
+        Console.WriteLine("Число нечетное");
+    } 
+
