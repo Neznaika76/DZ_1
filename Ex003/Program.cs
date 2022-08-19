ef2640aa297fe68e091183da9e7e500e0c38b710
Pavel <n9032692530@gmail.com>
2022-08-19 20:41:37 +0300

    Задание №4

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, Pasha");
+﻿int x;
+Console.WriteLine("Введите первое число:");
+x=Convert.ToInt32(Console.ReadLine());
+int y;
+Console.WriteLine("Введите второе число:");
+y=Convert.ToInt32(Console.ReadLine());
+int z;
+Console.WriteLine("Введите третье число:");
+z=Convert.ToInt32(Console.ReadLine());
+if (x>y)
+    {
+        if (x>z)
+        {
+            Console.Write("Максимальное число ");
+            Console.WriteLine(x);
+        }
+        else 
+        {
+            Console.Write("Максимальное число ");
+            Console.WriteLine(z);
+        }
+    }
+else
+   {
+        if (y>z)
+        {
+            Console.Write("Максимальное число ");
+            Console.WriteLine(y);
+        }
+        else 
+        {
+            Console.Write("Максимальное число ");
+            Console.WriteLine(z);
+        }
+     } 
