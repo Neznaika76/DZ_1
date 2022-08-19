52046eeddc04ecbdfb6fa12d7a0d3b61c7f01364
Pavel <n9032692530@gmail.com>
2022-08-19 20:27:01 +0300

    Задание №2

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, Pasha!");
+﻿int x;
+Console.WriteLine("Введите первое число:");
+x=Convert.ToInt32(Console.ReadLine());
+int y;
+Console.WriteLine("Введите второе число:");
+y=Convert.ToInt32(Console.ReadLine());
+if (x>y)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+else
+   {
+        Console.WriteLine("Второе число больше первого");
+    } 
